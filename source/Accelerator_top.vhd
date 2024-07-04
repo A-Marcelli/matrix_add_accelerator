9d9f30fc1228158e8227fea5ceeb8cf0d41579d2
Andrea Marcelli <marcelli.1901726@studenti.uniroma1.it>
2024-07-04 18:34:59 +0200

    template

---
+++ source/Accelerator_top.vhd
@@ -0,0 +1,40 @@
+-- ieee packages ------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+--use ieee.std_logic_misc.all;
+--use ieee.math_real.all;
+--use std.textio.all;
+
+
+entity matrix_add_accelerator is
+	generic(
+
+
+		 );
+  port (
+
+
+  	);
+ 
+ end entity matrix_add_accelerator;
+
+ 
+
+ architecture mat_acc of matrix_add_accelerator is 
+ 	--constants
+
+
+ 	--subtypes
+
+
+ 	--signals
+
+
+ 	--components
+
+ 	begin
+
+
+
+ end mat_acc;
