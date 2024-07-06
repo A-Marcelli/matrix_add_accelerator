9e763a35a207d79d9956bd1514d10fc87ec54022
Andrea Marcelli <marcelli.1901726@studenti.uniroma1.it>
2024-07-06 12:33:39 +0200

    immagine base e porte

--- source/Accelerator_top.vhd
+++ source/Accelerator_top.vhd
@@ -14,7 +14,19 @@ entity matrix_add_accelerator is
 
 
 		 );
-  port (
+  	port (
+  		clk                : in    std_logic;
+  		rst_in             : in    std_logic;
+  		----------------------------------------------------------------------------
+  		cpu_acc_data       : inout std_logic_vector(31 downto 0); -- input = comandi e scrittura su registri, output = lettura da parte della cpu
+  		cpu_acc_address    : in    std_logic_vector(31 downto 0);
+  		cpu_acc_read       : in    std_logic;                     -- write strobe
+  		cpu_acc_write      : in    std_logic;                     -- read  strobe
+  		----------------------------------------------------------------------------
+  		mem_acc_address    : out   std_logic_vector(31 downto 0);
+  		mem_acc_data       : inout std_logic_vector(31 downto 0); -- input = lettura da memoria, output = scrittura in memoria
+  		mem_acc_read       : out   std_logic;                     -- write strobe
+  		mem_acc_write      : out   std_logic                      -- read  strobe
 
 
   	);
