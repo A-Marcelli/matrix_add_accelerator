46fe54c67fda2b129a6816d50819eaac9d197c37
Marco <marco.pisani00@gmail.com>
2024-07-09 12:35:49 +0200

    Creato modulo SPM

--- source/Accelerator_top.vhd
+++ source/Accelerator_top.vhd
@@ -2,17 +2,19 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.math_real.all;
 use work.Byte_Busters.all;
 
 
 entity matrix_add_accelerator is
 	generic(
-		SPM_NUM        : natural := 4;   -- The number of scratchpads available "Minimum allowed is two"
-    	Addr_Width     : natural := 14;  -- This address is for scratchpads. Setting this will make the size of the spm to be: "2^Addr_Width -1"
-    	SPM_STRT_ADDR  : std_logic_vector(31 downto 0) := x"1000_0000";  -- This is starting address of the spms, it shouldn't overlap any sections in the memory map
-    	SIMD           : natural := 1    -- Changing the SIMD, would change the number of the functional units in the dsp, and the number of banks in the spms (can be power of 2 only e.g. 1,2,4,8)
-
-
+		SPM_NUM        : natural := 4;   -- The number of scratchpads and adders avaibles, "Minimum allowed is two" "MAX 255"
+    	Addr_Width     : natural := 14;  -- This address is for scratchpads banks. min: 4, max: 16 
+    	SIMD           : natural := 1;    -- Changing the SIMD, changes the number of banks in the spms. min 1, max 255
+        
+        N_RAM_ADDR     : natural := 3;     --MP, number of registers that contain a RAM cell address
+        N_LOCAL_ADDR   : natural := 3      --MP, number of registers that contain a local memory cell address
+        
 		 );
   	port (
   		clk                : in    std_logic;
@@ -37,6 +39,7 @@ entity matrix_add_accelerator is
 
  architecture mat_acc of matrix_add_accelerator is 
  	--constants
+    constant SPM_ADDR_LEN: natural := ADDR_WIDTH + integer(ceil(log2(real(SIMD))));
 
 
  	--signals
--- source/Local_memory.vhd
+++ source/Local_memory.vhd
@@ -2,35 +2,56 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.math_real.all;
 use work.Byte_Busters.all;
 
 entity scratchpad_memory is 
 	generic (
---    	SIMD           : natural;
-		SPM_NUM        : natural;
-    	ADDR_WIDTH     : natural
+    	SIMD           : natural;
+    	ADDR_WIDTH     : natural;
+    	ADDR_LEN       : natural 
 
 	);
 	port (
-	   data_in     : in    std_logic_vector((ELEMENT_SIZE-1) downto 0);        --MP
-	   data_out    : out   std_logic_vector((ELEMENT_SIZE-1) downto 0);        --MP
+	   data_out   : out   array_2d(1 downto 0)((ELEMENT_SIZE-1) downto 0);    -- da local a acceleratore
+	   data_in    : in    std_logic_vector((ELEMENT_SIZE-1) downto 0);        -- da acceleratore a local
 	   
-	   addr_in     : in    std_logic_vector((ADDR_WIDTH-1) downto 0);          --MP
-	   addr_out    : in    std_logic_vector((ADDR_WIDTH-1) downto 0);          --MP
+	   addr_out   : in    array_2d(1 downto 0)((ADDR_LEN-1) downto 0);          --MP
+	   addr_in    : in    std_logic_vector((ADDR_LEN-1) downto 0);          --MP
 	   
 	   read, write : in    std_logic;                                          --MP
 	   
-	   clk, reset  : in    std_logic                                           --MP
+	   clk        : in    std_logic                                           --MP
 	);
 end scratchpad_memory;
 
 architecture memory of scratchpad_memory is
 
-
-
+    signal mem: array_3d(SIMD-1 downto 0)((2**(ADDR_WIDTH)-1) downto 0)(ELEMENT_SIZE-1 downto 0);
 begin
 
+write_logic: process(clk)
+begin
+    if(rising_edge(clk)) then
+        if write = '1' then
+            mem( to_integer(unsigned(addr_in( (ADDR_LEN -1) downto ADDR_WIDTH))) )
+                (to_integer(unsigned(addr_in(ADDR_WIDTH-1 downto 0)))) <= data_in;       
+        end if;
+    end if;
+end process;
 
+read_logic: process(clk)
+begin
+    if(rising_edge(clk)) then
+        if read = '1' then
+            data_out(0) <= mem( to_integer(unsigned(addr_out(0)( (ADDR_LEN -1) downto ADDR_WIDTH))) )
+                (to_integer(unsigned(addr_out(0)(ADDR_WIDTH-1 downto 0))));
+            
+            data_out(1) <= mem( to_integer(unsigned(addr_out(1)( (ADDR_LEN -1) downto ADDR_WIDTH))) )
+                (to_integer(unsigned(addr_out(1)(ADDR_WIDTH-1 downto 0))));
+        end if; 
+    end if;
+end process;
 
 
 end memory;
