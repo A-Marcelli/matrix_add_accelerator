e7566f0e487368c3d2da54957a1e1d07dcb03989
Marco <marco.pisani00@gmail.com>
2024-07-28 16:45:12 +0200

    Script matlab per generare e sommare le matrici

--- script_matlab/Genera_Matrici.m
+++ script_matlab/Genera_Matrici.m
@@ -1,6 +1,9 @@
-% Genera dimensioni casuali per la matrice
-rows = randi([1, 100]);  % Numero di righe casuale tra 1 e 10
-cols = randi([1, 100]);  % Numero di colonne casuale tra 1 e 10
+% % Genera dimensioni casuali per la matrice
+% rows = randi([1, 50]);  % Numero di righe casuale tra 1 e 10
+% cols = randi([1, 50]);  % Numero di colonne casuale tra 1 e 10
+
+rows = 39;
+cols = 18;
 
 % Crea una matrice casuale di interi
 A = randi([1, 100], rows, cols);  % Matrice con valori casuali tra 1 e 100
@@ -34,6 +37,14 @@ for i = 1:rows
     end
 end
 % Messaggio di conferma
+
 disp(['Matrice B casuale di dimensioni ', num2str(rows), 'x', num2str(cols), ' salvata nel file ', filename]);
 
-
+sum=A+B;
+filename = 'somma.txt';
+fileID = fopen(filename, 'w');
+for i = 1:rows
+    for j = 1:cols
+        fprintf(fileID, '%d\n', sum(i, j));
+    end
+end
